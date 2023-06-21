b6d66929c2652dd96001ad4999ee3c1b9e439769
shueja-personal <j.a.shue@outlook.com>
2023-06-20 22:34:53 -0700

    added toggleable config mode

--- src/lib/select/SelectionLayer.svelte
+++ src/lib/select/SelectionLayer.svelte
@@ -2,26 +2,25 @@
     import {layout} from '../config';
     import type {DashboardTab, DashboardElement} from '../config'
     import { derived, get, writable, type Writable } from "svelte/store";
-    import {onMount} from "svelte"
     import Grid from "svelte-grid";
     import gridHelp from "svelte-grid/build/helper/index.mjs";
     import { widgetDefinitions } from '../widgets';
-	import { element } from 'svelte/internal';
-    import NT from "../util/NT"
 	import WidgetWrapper from './WidgetWrapper.svelte';
     export let tab : DashboardTab;
-    export let selectedIndex = 0;
+    export let selectedStore: Writable<string>;
+    export let enableSelection: Writable<boolean>;
 	
-    let selectedStore = writable("");
     const createGridConfigStore = (element:DashboardElement, uuid:string) => {
         return derived(
-            [selectedStore, element.type, element.layout.x, element.layout.y, element.layout.height, element.layout.width], 
-            ([selected, type, x, y, height, width]) => {
+            [enableSelection, selectedStore,
+                element.type, element.layout.x, element.layout.y,
+                element.layout.height, element.layout.width], 
+            ([enableSelection, selected, type, x, y, height, width]) => {
                 console.log(type, x, y, width, height)
                 return {x: (x??1)-1, y: (y??1)-1, h:height, w:width, 
-                    fixed:(selected !== uuid),
-                    draggable:(selected === uuid),
-                    resizable:(selected === uuid),
+                    fixed:(selected !== uuid) || !enableSelection,
+                    draggable:(selected === uuid) && enableSelection,
+                    resizable:(selected === uuid) && enableSelection,
                     id:uuid,
                     min: {w: widgetDefinitions[type]?.config.layout?.minWidth,
                     h: widgetDefinitions[type]?.config.layout?.minHeight},
@@ -67,7 +66,7 @@ console.log(tab.elements)
 
 <Grid bind:items={modifiedItems} rowHeight={50} gap={[0,0]} let:dataitem let:item {cols} on:pointerup={handleChange}>
     <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <div style="background:red" class="h-full w-full" on:mousedown={()=>$selectedStore = item.id}>
+    <div style="background:red" class="h-full w-full" on:mouseover={()=>$selectedStore = item.id} on:mousedown={()=>$selectedStore = item.id}>
         {#if true} <!--we need this block for the consts-->
         {@const config = tab.elements[item.id]} 
        <WidgetWrapper {config}></WidgetWrapper>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 
     export const prerender = true
     export const ssr = false;
-	import { get } from "svelte/store";
+	import { get, writable } from "svelte/store";
     import {loadLayoutFromJSON, layout} from "../lib/config"
     import ConfigPanel from "../lib/config/ConfigPanel.svelte"
 	import SelectionLayer from "$lib/select/SelectionLayer.svelte";
@@ -50,24 +50,17 @@
     }
 
     let inConfigMode = false;
+    let selectedStore = writable("")
+    let enableSelection= writable(false)
+
 </script>
 
 <div class="h-full w-full flex">
-    <ConfigPanel layout={layout} selectedTab={0} expanded={inConfigMode}></ConfigPanel>
+    <ConfigPanel layout={layout} selectedTab={0} expanded={$enableSelection}></ConfigPanel>
     <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <div class="flex-shrink w-[10px] h-full bg-slate-800" on:click={()=>inConfigMode = !inConfigMode}> >>></div>
+    <div class="flex-shrink w-[10px] h-full bg-slate-800" on:click={()=>$enableSelection = !$enableSelection}> >>></div>
     <div style="flex-grow:1">
-        <SelectionLayer tab={Object.values(layout)[0]} ></SelectionLayer>
-        <!-- <GridLayout cellHeight={50} cellWidth={50} showLines={true}>
-
-             <DashboardRenderer layout={layout}></DashboardRenderer>
-             {#if inConfigMode}
-            <div style="position:absolute; top:0; left:0; width:{50 * 50}px; height: {50 * 50}">
-                <SelectionLayer tab={subStore(layout, l=>l.tabs[0])} bind:selectedIndex></SelectionLayer>
-            </div>
-            {/if}
-            
-        </GridLayout> -->
+        <SelectionLayer {enableSelection} {selectedStore} tab={Object.values(layout)[0]} ></SelectionLayer>
     </div>
 
 </div>
